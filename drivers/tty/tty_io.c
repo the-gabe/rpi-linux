27228f8ded1ee6177715299247579cf9596beb19
Matt Brown <matt@nmatt.com>
2017-05-29 17:38:00 -0400

    security: tty: make TIOCSTI ioctl require CAP_SYS_ADMIN
    This introduces the tiocsti_restrict sysctl, whose default is controlled
    via CONFIG_SECURITY_TIOCSTI_RESTRICT. When activated, this control
    restricts all TIOCSTI ioctl calls from non CAP_SYS_ADMIN users.
    
    This patch depends on patch 1/2
    
    This patch was inspired from GRKERNSEC_HARDEN_TTY.
    
    This patch would have prevented
    https://bugzilla.redhat.com/show_bug.cgi?id=1411256 under the following
    conditions:
    * non-privileged container
    * container run inside new user namespace
    
    Possible effects on userland:
    
    There could be a few user programs that would be effected by this
    change.
    See: <https://codesearch.debian.net/search?q=ioctl%5C%28.*TIOCSTI>
    notable programs are: agetty, csh, xemacs and tcsh
    
    However, I still believe that this change is worth it given that the
    Kconfig defaults to n. This will be a feature that is turned on for the
    same reason that people activate it when using grsecurity. Users of this
    opt-in feature will realize that they are choosing security over some OS
    features like unprivileged TIOCSTI ioctls, as should be clear in the
    Kconfig help message.
    
    Threat Model/Patch Rational:
    
    >From grsecurity's config for GRKERNSEC_HARDEN_TTY.
    
     | There are very few legitimate uses for this functionality and it
     | has made vulnerabilities in several 'su'-like programs possible in
     | the past.  Even without these vulnerabilities, it provides an
     | attacker with an easy mechanism to move laterally among other
     | processes within the same user's compromised session.
    
    So if one process within a tty session becomes compromised it can follow
    that additional processes, that are thought to be in different security
    boundaries, can be compromised as a result. When using a program like su
    or sudo, these additional processes could be in a tty session where TTY
    file descriptors are indeed shared over privilege boundaries.
    
    This is also an excellent writeup about the issue:
    <http://www.halfdog.net/Security/2012/TtyPushbackPrivilegeEscalation/>
    
    When user namespaces are in use, the check for the capability
    CAP_SYS_ADMIN is done against the user namespace that originally opened
    the tty.
    
    Acked-by: Serge Hallyn <serge@hallyn.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Matt Brown <matt@nmatt.com>
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    Upstream-Status: Inappropriate

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -2257,6 +2257,7 @@ static int tty_fasync(int fd, struct file *filp, int on)
 }
 
 static bool tty_legacy_tiocsti __read_mostly = IS_ENABLED(CONFIG_LEGACY_TIOCSTI);
+static int tty_tiocsti_restrict __read_mostly = IS_ENABLED(CONFIG_SECURITY_TIOCSTI_RESTRICT);
 /**
  * tiocsti - fake input character
  * @tty: tty to fake input into
@@ -2278,6 +2279,12 @@ static int tiocsti(struct tty_struct *tty, u8 __user *p)
 	if (!tty_legacy_tiocsti && !capable(CAP_SYS_ADMIN))
 		return -EIO;
 
+	if (tty_tiocsti_restrict &&
+		!ns_capable(tty->owner_user_ns, CAP_SYS_ADMIN)) {
+		dev_warn_ratelimited(tty->dev,
+			"Denied TIOCSTI ioctl for non-privileged process\n");
+		return -EPERM;
+	}
 	if ((current->signal->tty != tty) && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
 	if (get_user(ch, p))
@@ -3619,6 +3626,15 @@ static const struct ctl_table tty_table[] = {
 		.maxlen		= sizeof(tty_ldisc_autoload),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "tiocsti_restrict",
+		.data		= &tty_tiocsti_restrict,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
 	},
