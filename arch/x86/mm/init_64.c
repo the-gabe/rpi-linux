e25696e16f61e34136dacdd916693a998506f6f7
Levente Polyak <levente@leventepolyak.net>
2019-01-13 21:42:45 +0100

    Revert "mark kernel_set_to_readonly as __ro_after_init"
        This commit causes CPA conflicts, cf.
        https://github.com/anthraxx/linux-hardened/issues/4.
    
        Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Upstream-Status: Inappropriate

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -1394,7 +1394,7 @@ void __init mem_init(void)
 	preallocate_vmalloc_pages();
 }
 
-int kernel_set_to_readonly __ro_after_init;
+int kernel_set_to_readonly;
 
 void mark_rodata_ro(void)
 {
@@ -1407,8 +1407,9 @@ void mark_rodata_ro(void)
 
 	printk(KERN_INFO "Write protecting the kernel read-only data: %luk\n",
 	       (end - start) >> 10);
-	kernel_set_to_readonly = 1;
 	set_memory_ro(start, (end - start) >> PAGE_SHIFT);
+
+	kernel_set_to_readonly = 1;
 
 	/*
 	 * The rodata/data/bss/brk section (but not the kernel text!)
